4fc5a2e249c0023356e0be2a01d38fe7aa0d6b57
Le Thu Huong <lthuongvn@yahoo.com>
2018-08-13 20:18:50 +1200

    setup formidable to handle file upload. set directory to upload files is uploadDir. Listen to fileBegin event to keep file uploaded same name as original. Make it easier to test, and verify. Without this formidable uses a long name with some hashing in the file name

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const http = require('http');
+const formidable = require('formidable');
+const path = require('path');
+
+const form = new formidable.IncomingForm();
+form.encoding = 'utf-8';
+form.keepExtensions = true;
+form.type = 'multipart/form-data';
+form.uploadDir = path.resolve(__dirname, 'uploadDir');
 
 http.createServer((req, res) => {
     if(req.url === '/upload' && req.method.toLowerCase() === 'post') {
+        form.parse(req);
+        form.on('fileBegin', function(name, file) {
+            console.log(`fileBegin: name = ${name}, file = ${JSON.stringify(file)}`);
+            file.path = form.uploadDir + '/' + file.name;
+        });
     }
 }).listen(8081);
 
