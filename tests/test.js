362430f0bc0b3be766ff7b36f71277153e6acae8
Le Thu Huong <lthuongvn@yahoo.com>
2018-08-14 19:35:54 +1200

    Write unit test for wordOccurenceTable method

--- tests/test.js
+++ tests/test.js
@@ -1,10 +1,22 @@
 const fileParser = require('../fileParser');
 const path = require('path');
 
+test('test wordOccurenceTable method', () => {
+    const words = ['apple', 'orange', 'orange', 'blue', 'red'];
+    const actual = fileParser.wordOccurenceTable(words, {});
+    const expected = {
+        'apple': 1,
+        'orange': 2,
+        'blue': 1,
+        'red': 1
+    };
+    expect(actual).toEqual(expected);
+});
+
 test('parsing sample1.txt file', () => {
     const testDataFile = path.resolve(__dirname, 'data/sample1.txt');
     fileParser.parse(testDataFile).then(result => {
         expect(result.totalWordCount).toBe(6);
         expect(result.wordOccurences).toEqual({'1': 1, '2': 1, '3': 1, 'Line': 3});
     });
-  });
+});
